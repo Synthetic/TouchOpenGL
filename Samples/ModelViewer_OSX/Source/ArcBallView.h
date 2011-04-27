a7a44411506ae5d4183c202445f4984966515f5a
Jonathan Wight <jwight@mac.com>
2011-04-26 17:26:31 -0700

    Broke ArcBallView into own class.

---
+++ Samples/ModelViewer_OSX/Source/ArcBallView.h
@@ -0,0 +1,16 @@
+//
+//  ArcBallView.h
+//  ModelViewer_OSX
+//
+//  Created by Jonathan Wight on 04/26/11.
+//  Copyright 2011 Inkling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ArcBallView : NSView {
+    
+}
+
+@end
---
+++ Samples/ModelViewer_OSX/Source/ArcBallView.m
@@ -0,0 +1,14 @@
+//
+//  ArcBallView.m
+//  ModelViewer_OSX
+//
+//  Created by Jonathan Wight on 04/26/11.
+//  Copyright 2011 Inkling. All rights reserved.
+//
+
+#import "ArcBallView.h"
+
+
+@implementation ArcBallView
+
+@end
