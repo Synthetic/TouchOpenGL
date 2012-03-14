6de6c8173d9f0714cd9c02a78592a71ec479337a
Jonathan Wight <jwight@mac.com>
2012-03-14 11:01:17 -0700

    Added new shaders

--- Shaders/make.sh
+++ Shaders/make.sh
@@ -3,5 +3,6 @@
 rm Source/*
 
 SLOBGen Shaders/BlitTexture.fsh Shaders/Default.vsh --class CBlitProgram --output Source
-SLOBGen Shaders/BlitTextureRectangle.fsh Shaders/Default.vsh --class CBlitProgramRectangle --output Source
+SLOBGen Shaders/BlitTextureRectangle.fsh Shaders/Default.vsh --class CBlitRectangleProgram --output Source
 SLOBGen Shaders/Blur.fsh Shaders/Default.vsh --class CBlurProgram --output Source
+SLOBGen Shaders/CompositeTexture.fsh Shaders/Default.vsh --class CCompositeProgram --output Source
