f3df449a8d68be8311926c3bcc57fe40542f7e20
Jonathan Wight <jwight@mac.com>
2012-06-23 13:42:50 -0700

    Added blank demo app

---
+++ Samples/OSX/TouchOpenGLDemo/TouchOpenGLDemo/CAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CAppDelegate.h
+//  TouchOpenGLDemo
+//
+//  Created by Jonathan Wight on 6/23/12.
+//  Copyright (c) 2012 Jonathan Wight. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Samples/OSX/TouchOpenGLDemo/TouchOpenGLDemo/CAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  CAppDelegate.m
+//  TouchOpenGLDemo
+//
+//  Created by Jonathan Wight on 6/23/12.
+//  Copyright (c) 2012 Jonathan Wight. All rights reserved.
+//
+
+#import "CAppDelegate.h"
+
+@implementation CAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
