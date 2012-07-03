a20f8cbe81db2dc9429987f63fede117d7fa0022
Jonathan Wight <jwight@mac.com>
2012-07-03 14:04:37 -0700

    Unit Tests.

---
+++ Samples/OSX/TouchOpenGLDemo/UnitTests/UnitTests.h
@@ -0,0 +1,13 @@
+//
+//  UnitTests.h
+//  UnitTests
+//
+//  Created by Jonathan Wight on 7/3/12.
+//  Copyright (c) 2012 Jonathan Wight. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UnitTests : SenTestCase
+
+@end
--- Samples/OSX/TouchOpenGLDemo/UnitTests/UnitTests.m
+++ Samples/OSX/TouchOpenGLDemo/UnitTests/UnitTests.m
@@ -10,23 +10,23 @@
 
 @implementation UnitTests
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
+//- (void)setUp
+//{
+//    [super setUp];
+//    
+//    // Set-up code here.
+//}
+//
+//- (void)tearDown
+//{
+//    // Tear-down code here.
+//    
+//    [super tearDown];
+//}
 
 - (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in UnitTests");
-}
+    {
+//    STFail(@"Unit tests are not implemented yet in UnitTests");
+    }
 
 @end
